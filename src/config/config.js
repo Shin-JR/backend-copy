795dd8b7a2e10e309fc073506eaf94829402c8c9
Shin-JR <j.riquelme@uc.cl>
2023-12-01 19:13:09 -0300

    fix db name

--- src/config/config.js
+++ src/config/config.js
@@ -8,7 +8,7 @@ module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: `${process.env.DB_NAME}_development`,
+    database: `${process.env.DB_NAME}`,
     port: process.env.POSTGRESQL_PORT,
     host: process.env.DB_HOST,
     dialect: 'postgres'
@@ -16,14 +16,14 @@ module.exports = {
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: `${process.env.DB_NAME}_test`,
+    database: `${process.env.DB_NAME}`,
     host: process.env.DB_HOST,
     dialect: 'postgres'
   },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: `${process.env.DB_NAME}_production`,
+    database: `${process.env.DB_NAME}`,
     host: process.env.DB_HOST,
     dialect: 'postgres'
   }
